8369d5562c1edb88da3b89a5d2d4ad49f86ff34e
Ricciardo Simone <ricciardo855@gmail@com>
2022-07-05 16:26:52 +0200

    js + html

---
+++ js/main.js
@@ -0,0 +1,30 @@
+const kilometri = document.getElementById('kilometri').value;
+
+const eta = document.getElementById('eta').value;
+
+
+if (isNaN(kilometri) || isNaN(eta)){
+    alert('valori inseriti non validi')
+} else {
+    
+    let totalekm = kilometri * 0.21;
+    
+    
+    if(eta < 18) {
+        const sconto = totalekm / 100 * 20;
+        totalekm = totalekm - sconto
+        messaggio = 'il prezzo del biglietto è scontato del 20%. il prezzo finale è di € ' + totalekm.toFixed(2);
+        
+    } else if (eta > 65) {
+        const sconto = totalekm / 100 * 40;
+        totalekm = totalekm - sconto
+        messaggio = 'il prezzo del biglietto è scontato del 40%. il prezzo finale è di € ' + totalekm.toFixed(2);
+        
+    } else {
+        messaggio = 'il prezzo del biglietto è di € ' + totalekm;
+    }
+    
+    function myFunction() {
+        document.getElementById("demo").innerHTML = messaggio;
+      }
+}
