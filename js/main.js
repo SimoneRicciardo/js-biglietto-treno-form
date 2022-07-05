809e5a7858e9e6dc487be1e5c3789d532ca01d11
Ricciardo Simone <ricciardo855@gmail@com>
2022-07-05 17:34:30 +0200

    fix file js

--- js/main.js
+++ js/main.js
@@ -1,30 +1,34 @@
-const kilometri = document.getElementById('kilometri').value;
+let MainButton = document.getElementById('main-button');
+MainButton.addEventListener('click', function(){
 
-const eta = document.getElementById('eta').value;
+    let kilometri = document.getElementById('kilometri').value;
 
+    let eta = document.getElementById('eta').value;
 
-if (isNaN(kilometri) || isNaN(eta)){
-    alert('valori inseriti non validi')
-} else {
-    
-    let totalekm = kilometri * 0.21;
-    
-    
-    if(eta < 18) {
-        const sconto = totalekm / 100 * 20;
-        totalekm = totalekm - sconto
-        messaggio = 'il prezzo del biglietto è scontato del 20%. il prezzo finale è di € ' + totalekm.toFixed(2);
-        
-    } else if (eta > 65) {
-        const sconto = totalekm / 100 * 40;
-        totalekm = totalekm - sconto
-        messaggio = 'il prezzo del biglietto è scontato del 40%. il prezzo finale è di € ' + totalekm.toFixed(2);
-        
+    if (isNaN(kilometri) || isNaN(eta)){
+        alert('valori inseriti non validi')
     } else {
-        messaggio = 'il prezzo del biglietto è di € ' + totalekm;
+
+        let totalekm = kilometri * 0.21;
+        
+
+        if(eta < 18) {
+            const sconto = totalekm / 100 * 20;
+            totalekm = totalekm - sconto
+            messaggio = 'il prezzo del biglietto è scontato del 20%. il prezzo finale è di € ' + totalekm.toFixed(2);
+
+        } else if (eta > 65) {
+            const sconto = totalekm / 100 * 40;
+            totalekm = totalekm - sconto
+            messaggio = 'il prezzo del biglietto è scontato del 40%. il prezzo finale è di € ' + totalekm.toFixed(2);
+
+        } else {
+            messaggio = 'il prezzo del biglietto è di € ' + totalekm;
+        }
+
+
+        console.log(messaggio);
     }
+
     
-    function myFunction() {
-        document.getElementById("demo").innerHTML = messaggio;
-      }
-}
+});
